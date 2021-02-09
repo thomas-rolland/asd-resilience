39d294ebdc4015c3b583675075e3972076858fac
thomas-rolland <78741025+thomas-rolland@users.noreply.github.com>
2021-02-09 10:38:27 +0100

    Update linearOrdinalRegression.R

--- scripts/linearOrdinalRegression.R
+++ scripts/linearOrdinalRegression.R
@@ -4,60 +4,64 @@
 # Functions to calculate regression coefficient associated to variant presence
 #=========================================================================
 
+library(MASS)
+
 # Linear regression
-linear_regression = function(individual_table, formula)
+linear_regression = function(individual_table, variant_table, response, covariates)
 {
-df = matrix(nrow = 1, ncol = 4, data = NA)
+	# response = column containing response variable
+	# covariates = vector containing list of covariates (e.g. age, sex)
+	
+	# Adding column for variant presence
+	individual_table = cbind(individual_table, has_variant = "No")
+	individual_table$has_variant[individual_table$IID %in% variant_table$IID] = "Yes"
+	covariates = c("has_variant", covariates)
+	
+	df = matrix(nrow = length(covariates), ncol = 5, data = NA)
+	colnames(df) = c("covariate", "coefficient", "CI_lower", "CI_upper", "p.value")
+	
+	# Running regression analysis for specific outcome
+	formula = as.formula(paste(response, "~", paste(covariates, collapse = "+")))
+	model = glm(formula, data = individual_table, na.action = na.omit)
 
-  # if (dim(tmp[tmp$variant == 1,])[1] == 0)
-  #   return (df)
+	# Get regression coefficients, CIs and p-values
+	for (i in 1:length(covariates))
+		df[i, ] = c(covariates[i], summary(model)$coefficients[grepl(covariates[i], row.names(summary(model)$coefficients)),1], as.double(conf_int(model)[grepl(covariates[i], row.names(summary(model)$coefficients)),]), summary(model)$coefficients[grepl(covariates[i], row.names(summary(model)$coefficients)), 4])
 
-  # Running regression analysis
-  model = glm(form, data = tmp, na.action = na.omit)
-
-  # Returning coefficient, CI and p-value
-  for (i in 1:length(covariates))
-  {
-    df[i, ] = c(summary(model)$coefficients[grepl(covariates[i], row.names(summary(model)$coefficients)),1], as.double(conf_int(model)[grepl(covariates[i], row.names(summary(model)$coefficients)),]), summary(model)$coefficients[grepl(covariates[i], row.names(summary(model)$coefficients)), 4])
-  }
-  df = as.data.frame(df)
-
-  return (df)
+	return (as.data.frame(df))
 }
 
 
-#################
 # Ordinal regression
-#################
-ordinal_regression = function(t_indiv, form, populations, status, covariates)
+ordinal_regression = function(individual_table, variant_table, response, covariates)
 {
-  df = matrix(nrow = length(covariates), ncol = 4, data = NA)
+	# response = column containing response variable
+	# covariates = vector containing list of covariates (e.g. age, sex)
 
-  # Formatting data frame
-  tmp = t_indiv[t_indiv$pop %in% populations & t_indiv$status == status, ]
-
-  # if (dim(tmp[tmp$variant == 1,])[1] == 0)
-  #   return (df)
-
-  # Running regression
-  m <- polr(form, data = tmp, Hess = TRUE, na.action = na.omit)
-  ctable <- coef(summary(m))
+	# Adding column for variant presence
+	individual_table = cbind(individual_table, has_variant = "No")
+	individual_table$has_variant[individual_table$IID %in% variant_table$IID] = "Yes"
+	covariates = c("has_variant", covariates)
+	
+	df = matrix(nrow = length(covariates), ncol = 5, data = NA)
+	colnames(df) = c("covariate", "coefficient", "CI_lower", "CI_upper", "p.value")
+	
+	# Running regression
+	m <- polr(form, data = tmp, Hess = TRUE, na.action = na.omit)
+	ctable <- coef(summary(m))
 	p <- pnorm(abs(ctable[, "t value"]), lower.tail = FALSE) * 2
 	ctable <- cbind(ctable, "p value" = p)
 	ci = conf_int(m)
-  ctable = cbind(OR = exp(coef(m)) - 1, exp(ci) - 1, pval = p[names(p) %in% covariates])
+	ctable = cbind(OR = exp(coef(m)) - 1, exp(ci) - 1, pval = p[names(p) %in% covariates])
 
-  # Returning coefficient, CI and p-value
-  for (i in 1:length(covariates))
-  {
-    row = NA
-    for (row in 1:length(row.names(ctable)))
-      if (grepl(covariates[i], row.names(ctable)[row]) & ! grepl(":", row.names(ctable)[row]))
-        break
+	# Get regression coefficients, CIs and p-values
+	for (i in 1:length(covariates))
+	{
+		for (row in 1:length(row.names(ctable)))
+			if (grepl(covariates[i], row.names(ctable)[row]) & ! grepl(":", row.names(ctable)[row]))
+				break
+		df[i, ] = c(covariates[i], ctable[row, ])
+	}
 
-    df[i, ] = ctable[row, ]
-  }
-  df = as.data.frame(df)
-
-  return (df)
+	return (as.data.frame(df))
 }

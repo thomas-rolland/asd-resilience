f67e6b2b6b14effaecdf0d012981bcf54005966f
thomas-rolland <78741025+thomas-rolland@users.noreply.github.com>
2021-02-09 09:44:27 +0100

    Update attributableRiskRelativeRisk.R

--- scripts/attributableRiskRelativeRisk.R
+++ scripts/attributableRiskRelativeRisk.R
@@ -1,108 +1,92 @@
 
+#=========================================================================
+# attributableRiskRelativeRisk.R
+# Functions to calculate AR and RR with confidence intervals
+#=========================================================================
+
 # Number of individuals carrying variants per gene
-variant2gene = function(variant_table, sample_table)
+variant2gene = function(variant_table, sample_table, gene_table)
 {
-  # Creating data frame per gene, stratified by population and status
-	tab = as.data.frame(cbind(expand.grid(t_gene$gene, unique(total_individuals$population), unique(total_individuals$status)), NC = 0, N = 0, tada = NA, LOEUF = NA, pLI = NA))
-  colnames(tab) = c("gene", "population", "status", "NC", "N", "tada", "LOEUF", "pLI")
+	# Creating dataframe per gene, stratified by cohort and status
+	tab_gene = as.data.frame(cbind(expand.grid(gene_table$gene_symbol, unique(sample_table$cohort), unique(sample_table$status)), NC = 0, N = 0))
+  	colnames(tab_gene) = c("gene", "cohort", "status", "NC", "N")
 
-  # Formatting columns
-  tab$gene = as.character(tab$gene)
-  tab$population = as.character(tab$population)
-  tab$status = as.character(tab$status)
-	tab$N = as.double(tab$N)
-	tab$NC = as.double(tab$NC)
+	# Formatting columns
+	tab_gene$gene = as.character(tab_gene$gene)
+	tab_gene$cohort = as.character(tab_gene$cohort)
+	tab_gene$status = as.character(tab_gene$status)
+	tab_gene$N = as.double(tab_gene$N)
+	tab_gene$NC = as.double(tab_gene$NC)
 
-  # Calculate number of variants per gene stratified by status
-  for (gene in unique(tab$gene))
+	# Calculate number of variants per gene stratified by cohort and status
+	for (gene in unique(tab_gene$gene))
 	{
-    for (population in unique(tab$population))
-  	{
-      for (status in unique(tab$status))
-      {
-        # Number of carriers and number of individuals
-  			tab$NC[tab$gene == gene & tab$population == population & tab$status == status] = length(unique(t$IID[t$gene == gene & t$population == population & t$status == status]))
-  			tab$N[tab$gene == gene & tab$population == population & tab$status == status] = total_individuals$N[total_individuals$population == population & total_individuals$status == status]
-  	  }
-  	}
-  }
-
-  # Formatting columns
-  tab$gene = as.character(tab$gene)
-  tab$NC = as.integer(tab$NC)
-  tab$N = as.integer(tab$N)
-	tab$tada = as.double(tab$tada)
-	tab$LOEUF = as.double(tab$LOEUF)
-	tab$pLI = as.double(tab$pLI)
-
-	return (tab)
+		for (cohort in unique(tab_gene$cohort))
+  		{
+			for (status in unique(tab_gene$status))
+			{
+				tab_gene$NC[tab_gene$gene == gene & tab_gene$cohort == cohort & tab_gene$status == status] = length(unique(variant_table$IID[variant_table$gene == gene & variant_table$cohort == cohort & variant_table$status == status]))
+				tab_gene$N[tab_gene$gene == gene & tab_gene$cohort == cohort & tab_gene$status == status] = sample_table$N[sample_table$cohort == cohort & sample_table$status == status]
+			}
+		}
+	}
+	
+	return (tab_gene)
 }
 
 
-#################
-# Measure of penetrance and relative/attributable risk
-#################
-gene2penetrance = function(genes, total_individuals, Inf_RR = TRUE, rm_noCase = FALSE)
+# Calculate attributable and relative risk
+gene2risk = function(tab_gene, sample_table, RR_Inf = TRUE, RR_zero = FALSE)
 {
-	tab = as.data.frame(cbind(gene = as.character(unique(genes$gene)), tada = NA, LOEUF = NA, pLI = NA, aff = 0, unaff = 0, penetrance = 0, penetrance_sd = 0, RR = 0, logRR_CI_low = 0, logRR_CI_high = 0, AR = 0, AR_CI_low = 0, AR_CI_high = 0))
+	# RR_Inf = Boolean indicating whether infinite relative risk values due to absent carrier controls should be artificially set to 1 to avoid infinite RR
+	# RR_zero = Boolean indicating whether genes for which no carrier cases were identified should be removed
+	
+	tab_risk = as.data.frame(cbind(gene = as.character(unique(tab_gene$gene)), case = 0, control = 0, RR = 0, logRR_CI_low = 0, logRR_CI_high = 0, AR = 0, AR_CI_low = 0, AR_CI_high = 0))
 
-  # Format columns
-  tab$gene = as.character(tab$gene)
-  for (k in 2:dim(tab)[2])
-    tab[,k] = as.double(tab[,k])
+	# Format columns
+	tab_risk$gene = as.character(tab_risk$gene)
+	for (k in 2:dim(tab_risk)[2])
+		tab_risk[,k] = as.double(tab_risk[,k])
 
-  # Measure penetrance and RR/AR
-	for (gene in unique(tab$gene))
+	# Calculate AR/RR
+	for (gene in unique(tab_risk$gene))
 	{
-    # Get deleteriousness scores
-    tab$tada[tab$gene == gene] = genes$tada[genes$gene == gene & genes$status == "Case"]
-    tab$LOEUF[tab$gene == gene] = genes$LOEUF[genes$gene == gene & genes$status == "Case"]
-    tab$pLI[tab$gene == gene] = genes$pLI[genes$gene == gene & genes$status == "Case"]
+		IE = tab_gene$NC[tab_gene$gene == gene & tab_gene$status == "Case"]
+		CE = tab_gene$NC[tab_gene$gene == gene & tab_gene$status == "Control"]
+		IN = tab_gene$N[tab_gene$gene == gene & tab_gene$status == "Case"] - IE
+		CN = tab_gene$N[tab_gene$gene == gene & tab_gene$status == "Control"] - CE
 
-    # Number of carriers and non-carriers
-  	IE = genes$NC[genes$gene == gene & genes$status == "Case"]
-		CE = genes$NC[genes$gene == gene & genes$status == "Control"]
-		IN = genes$N[genes$gene == gene & genes$status == "Case"] - IE
-		CN = genes$N[genes$gene == gene & genes$status == "Control"] - CE
-
-    # If no control found, add one manually so that penetrance/RR are not infinite
-    if (Inf_RR == FALSE & CE == 0 & IE > 0)
+		# If no control carriers, set CE to 1 so that relative risk is not infinite
+		if (RR_Inf == FALSE & CE == 0 & IE > 0)
 		{
 			CE = 1
-			CN = total_individuals$N[total_individuals$status == "Control"] - CE
+			CN = sample_table$N[sample_table$status == "Control"] - CE
 		}
+    
+		# Fraction of cases and controls with variant
+		tab_risk$case[tab_risk$gene == gene] = IE / (IE + IN)
+		tab_risk$control[tab_risk$gene == gene] = CE / (CE + CN)
 
-    # Fraction of cases and controls with variant
-		tab$aff[tab$gene == gene] = IE / (IE + IN)
-		tab$unaff[tab$gene == gene] = CE / (CE + CN)
-
-    # Measure penetrance
-    if (tab$aff[tab$gene == gene] + tab$unaff[tab$gene == gene] > 0)
-    {
-		  tab$penetrance[tab$gene == gene] = tab$aff[tab$gene == gene] / (tab$aff[tab$gene == gene] + tab$unaff[tab$gene == gene])
-      tab$penetrance_sd[tab$gene == gene] = sqrt((tab$penetrance[tab$gene == gene] * (1 - tab$penetrance[tab$gene == gene])) / (tab$aff[tab$gene == gene] + tab$unaff[tab$gene == gene]))
-    }
-
-    # Measure RR and AR
-    RR = (IE / (IE + IN)) / (CE / (CE + CN))
-		RD = (IE / (IE + IN)) - (CE / (CE + CN))
+		# Measure AR and RR
+		RR = (IE / (IE + IN)) / (CE / (CE + CN))
+		AR = (IE / (IE + IN)) - (CE / (CE + CN))
 		logRR_SE = sqrt((IN / (IE * (IE + IN))) + (CN / (CE * (CE + CN))))
-		RD_SE = sqrt(((IE * IN) / (IE + IN)^3) + ((CE * CN) / (CE + CN)^3))
-		tab$RR[tab$gene == gene] = RR
-		tab$logRR_CI_low[tab$gene == gene] = log(RR) - logRR_SE * 0.95
-		tab$logRR_CI_high[tab$gene == gene] = log(RR) + logRR_SE * 0.95
-		tab$AR[tab$gene == gene] = RD
-		tab$AR_CI_low[tab$gene == gene] = RD - RD_SE * 0.95
-		tab$AR_CI_high[tab$gene == gene] = RD + RD_SE * 0.95
+		AR_SE = sqrt(((IE * IN) / (IE + IN)^3) + ((CE * CN) / (CE + CN)^3))
+		tab_risk$RR[tab_risk$gene == gene] = RR
+		tab_risk$logRR_CI_low[tab_risk$gene == gene] = log(RR) - logRR_SE * 0.95
+		tab_risk$logRR_CI_high[tab_risk$gene == gene] = log(RR) + logRR_SE * 0.95
+		tab_risk$AR[tab_risk$gene == gene] = AR
+		tab_risk$AR_CI_low[tab_risk$gene == gene] = AR - AR_SE * 0.95
+		tab_risk$AR_CI_high[tab_risk$gene == gene] = AR + AR_SE * 0.95
 	}
 
-  # Remove genes for which no cases and no controls were found
-  tab = tab[! is.na(tab$RR),]
+	# Remove genes for which no case and no control carriers were found
+	tab_risk = tab_risk[! is.na(tab_risk$RR),]
 
-  # Remove genes for which no cases were found
-  if (rm_noCase == TRUE)
-    tab = tab[tab$RR > 0,]
+	# Remove genes for which no case carriers were found
+	if (RR_zero == FALSE)
+		tab_risk = tab_risk[tab_risk$RR > 0,]
 
-	return (tab)
+	return (tab_risk)
 }
 

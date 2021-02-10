de22db7b345b4a3d62228feb8fbd32419a4bad71
thomas-rolland <78741025+thomas-rolland@users.noreply.github.com>
2021-02-10 12:15:36 +0100

    Update distributionIndividualsGPS.R

--- scripts/distributionIndividualsGPS.R
+++ scripts/distributionIndividualsGPS.R
@@ -8,35 +8,39 @@
 # Quantiles of values, not of individuals
 value_quantile = function(distrib, n_quantiles)
 {
-  mini = min(distrib, na.rm = TRUE)
-  maxi = max(distrib, na.rm = TRUE)
-  quant = (maxi - mini) / n_quantiles
-  q = seq(mini, maxi, quant)
-  q = c(q[1], q[length(q) - 1], q[length(q)])
-  q[length(q)] = maxi + 1
-  return (q)
+	mini = min(distrib, na.rm = TRUE)
+	maxi = max(distrib, na.rm = TRUE)
+	quant = (maxi - mini) / n_quantiles
+	q = seq(mini, maxi, quant)
+	q = c(q[1], q[length(q) - 1], q[length(q)])
+	q[length(q)] = maxi + 1
+	return (q)
 }
 
 # Get fraction of individuals in each quantile
-distrib_individual_GPS = function(individual_table, n_quantiles = 3, cohort, status, transmission)
+distrib_individual_GPS = function(variant_table, asd_gps_table, n_quantiles = 3, carriers = TRUE)
 {
 	# n_quantiles = Number of quantiles to split the distribution
-	# cohort = Cohort of interest
-	# status = Status of interest ("case", "control")
-	# transmission = Vector of transmission types to consider (NA for non-carriers, "denovo", "father", "mother", "unknown")
-	
-	individual_table = individual_table[! is.na(individual_table$ASD_GPS),]
-	
+	# carriers = Whether carriers or non-carriers are considered (boolean)
+
+	asd_gps_table = asd_gps_table[! is.na(asd_gps_table$asd_gps),]
+
 	# Get quantiles based on values, not on distribution of individuals
-	q = value_quantile(individual_table$ASD_GPS, n_quantiles)
- 
+	q = value_quantile(asd_gps_table$asd_gps, n_quantiles)
+
 	n = rep(NA, length(q) - 1)
 	f = rep(NA, length(q) - 1)
 	sep = rep(NA, length(q) - 1)
-	
-	# Get number of individuals with status of interest, in cohort of interest, and with/without LoF with given transmission type, in each quantile
+
+	# Restrict individuals to those carrying or not carrying variant
+	if (carriers == TRUE)
+		asd_gps_table = asd_gps_table[asd_gps_table$iid %in% variant_table$iid,]
+	else
+		asd_gps_table = asd_gps_table[! asd_gps_table$iid %in% variant_table$iid,]
+
+	# Get number of individuals in quantiles
 	for (i in 2:length(q))
-		n[i - 1] = length(unique(as.character(individual_table$iid[individual_table$ASD_GPS >= q[i - 1] & individual_table$ASD_GPS < q[i] & individual_table$cohort == cohort & individual_table$status == status & individual_table$transmission %in% transmission])))
+		n[i - 1] = length(unique(as.character(asd_gps_table$iid[asd_gps_table$asd_gps >= q[i - 1] & asd_gps_table$asd_gps < q[i]])))
 
 	# Get fraction of individuals in each quantile, and corresponding standard errors of proportion
 	for (i in 2:length(q))
@@ -45,9 +49,8 @@ distrib_individual_GPS = function(individual_table, n_quantiles = 3, cohort, sta
 		sep[i - 1] = sqrt((f[i - 1] * (1 - f[i - 1])) / sum(n))
 	}
 
-	df = cbind(seq(2, length(q)), n, f, sep)
+	df = cbind(paste(seq(1, length(q) - 1), seq(2, length(q)), sep = "-"), n, f, sep)
 	colnames(df) = c("quantile", "n_individuals", "fraction_individuals", "standard_error")
 
 	return (as.data.frame(df))
 }
-
